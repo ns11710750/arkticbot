d61b65768f365e3db4c14ec4c2faba956f8f4b69
ns11710750 <54624620+ns11710750@users.noreply.github.com>
2019-09-01 00:10:16 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,65 +7,128 @@ bot.on('ready', () =>{
       });
 
 bot.on('message', msg=>{
-    if(msg.content === "arktic"){
+    if(msg.content.toLowerCase() === "arktic"){
     msg.reply('Arktic is the best server ever say CHEERS!');
 }
-else if(msg.content === "anzy"){
+else if(msg.content.toLowerCase() === "anzy"){
     msg.reply('SACHIN is Co founder of Arktic Clan');
 }
-else if(msg.content === "izen"){
+else if(msg.content.toLowerCase() === "izen"){
     msg.reply('SAGAR is Co founder of Arktic Clan');
 }
-else if(msg.content === "sarge"){
+else if(msg.content.toLowerCase() === "sarge"){
     msg.reply('Nisant Co founder of Arktic Clan');
 }
-else if(msg.content === "godfather"){
+else if(msg.content.toLowerCase() === "godfather"){
     msg.reply('ISHAAN is Co founder of Arktic Clan');
 }
-else if(msg.content === "yoyo"){
+else if(msg.content.toLowerCase() === "yoyo"){
     msg.reply('MITESH is Co founder of Arktic Clan');
 }
-else if(msg.content === "crusader"){
+else if(msg.content.toLowerCase() === "crusader"){
     msg.reply('AJAY is Co founder of Arktic Clan');
 }
-else if(msg.content === "beast"){
+else if(msg.content.toLowerCase() === "beast"){
     msg.reply('SASHIKANT is Co founder of Arktic Clan');
 }
-else if(msg.content === "chutiya"){
+else if(msg.content.toLowerCase() === "chutiya"){
     msg.reply('Bhen ke lawde gaand na fulao maa chood denge tumhari');
 }
-else if(msg.content === "bhenchod"){
+else if(msg.content.toLowerCase() === "bhenchod"){
     msg.reply('Bhosdike idhar gaali mat de lawde maa chod denge nahi to');
 }
-else if(msg.content === "madarchod"){
+else if(msg.content.toLowerCase() === "madarchod"){
     msg.reply('Bhosdike idhar gaali mat de lawde madarchod hoga tu');
 }
-else if(msg.content === "gandu"){
+else if(msg.content.toLowerCase() === "gandu"){
     msg.reply('Bhosdike idhar gaali mat de lawde, tera baap gandu samja');
 }
-else if(msg.content === "gaandu"){
+else if(msg.content.toLowerCase() === "gaandu"){
     msg.reply('Bhosdike idhar gaali mat de lawde, tera baap gandu samja');
 }
-else if(msg.content === "lawda"){
+else if(msg.content.toLowerCase() === "lawda"){
     msg.reply('Tu chakka hain kya kisne lund kaat diya tera?');
 }
-else if(msg.content === "lawde"){
+else if(msg.content.toLowerCase() === "lawde"){
     msg.reply('Tu chakka hain kya kisne lund kaat diya tera?');
 }
-else if(msg.content === "randi"){
+else if(msg.content.toLowerCase() === "randi"){
     msg.reply('Bhosdike idhar gaali mat de lawde, teri maa randi samja');
 }
-else if(msg.content === "randii"){
+else if(msg.content.toLowerCase() === "randii"){
     msg.reply('Bhosdike idhar gaali mat de lawde, teri maa randi samja');
 }
-else if(msg.content === "motherchodd"){
+else if(msg.content.toLowerCase() === "motherchodd"){
     msg.reply('Bhosdike idhar gaali mat de lawde madarchod hoga tu');
 }
-else if(msg.content === "behenchod"){
+else if(msg.content.toLowerCase() === "behenchod"){
     msg.reply('Bhosdike idhar gaali mat de lawde maa chod denge nahi to');
 }
-else if(msg.content === "chutia"){
+else if(msg.content.toLowerCase() === "chutia"){
     msg.reply('Bhen ke lawde gaand na fulao maa chood denge tumhar');
+}
+else if(msg.content.toLowerCase() === "!monster"){
+    msg.reply('\nDownload Monster Hack\nhttp://gestyy.com/w3388u');
+}
+else if(msg.content.toLowerCase() === "!creation"){
+    msg.reply('\nDownload Creation Hack\nhttps://cdn.discordapp.com/attachments/615456178562138153/615679362058027024/imgui_8-26-2019.exe');
+}
+else if(msg.content.toLowerCase() === "!degogh"){
+    msg.reply('\nDownload Degogh from\nhttps://linktr.ee/degogh');
+}
+else if(msg.content.toLowerCase() === "!fendi"){
+    msg.reply('\nDownload Fendi from \nhttp://gestyy.com/w33rN2');
+}
+else if(msg.content.toLowerCase() === "!kamikaze"){
+    msg.reply('\nDownload Kamikaze from \nhttps://mega.nz/#!2yI1FCwC!bYBK8B57yZk9AcLPG6BQBOu3nNBzzY71nr9lFzAg_24');
+}
+else if(msg.content.toLowerCase() === "!lamborghini"){
+    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!lb"){
+    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!lifevip"){
+    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
+}
+else if(msg.content.toLowerCase() === "!mh"){
+    msg.reply('\ndownload memory hackers from \nhttps://mega.nz/#!wVMQHAzJ!jYfqHDKVpm5CCQUjN6dnaAzIJ8AmLQjueRT2BTu9ouk');
+}
+else if(msg.content.toLowerCase() === "!sinki"){
+    msg.reply('\nDownload Sinki from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!vnhax"){
+    msg.reply('\nDownload vn hax from \nhttps://cdn.discordapp.com/attachments/592650786782314501/611159206824771586/VnHaxLoader-Win64-Shipping_4.exe');
+}
+else if(msg.content.toLowerCase() === "!youtube"){
+    msg.reply('HEY THIS IS MY YOUTUBE CHANNEL\nSUBSCRIBE AND GET DAILY HACK GAMEPLAY VIDEO\nhttps://www.youtube.com/channel/UC083eydMbfgPuHMja6rte9Q');
+}
+else if(msg.content.toLowerCase() === "loda"){
+    msg.reply('Bhaag na motherchod idhar maa chudane kyo aaya');
+}
+else if(msg.content.toLowerCase() === "lode"){
+    msg.reply('Bhaag na motherchod idhar maa chudane kyo aaya');
+}
+else if(msg.content.toLowerCase() === "fuck"){
+    msg.reply('Get Lost and Fuck yourself Bitch');
+}
+else if(msg.content.toLowerCase() === "fucck"){
+    msg.reply('Get Lost and Fuck yourself Bitch');
+}
+else if(msg.content.toLowerCase() === "fuckk"){
+    msg.reply('Get Lost and Fuck yourself Bitch');
+}
+else if(msg.content.toLowerCase() === "!errorx"){
+    msg.reply('\nDownload Errorx from \nhttps://cdn.discordapp.com/attachments/609707272527937537/617370377114353694/errorXvip.vmp.safe.exe');
+}
+else if(msg.content.toLowerCase() === "bhosdike"){
+    msg.reply('Bhosdike idhar gaali mat de lawde, Gaand mara jangal mein chutye');
+}
+else if(msg.content.toLowerCase() === "bhosdika"){
+    msg.reply('Bhosdike idhar gaali mat de lawde, Gaand mara jangal mein chutye');
+}
+else if(msg.content.toLowerCase() === "madrchod"){
+    msg.reply('Bhosdike idhar gaali mat de lawde madarchod hoga tu');
 }
 })
 
