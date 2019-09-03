7622745f149773eb3ff43211c8a61544441c5032
ns11710750 <54624620+ns11710750@users.noreply.github.com>
2019-09-03 13:59:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,57 @@ else if(msg.content.toLowerCase() === "crusader"){
 else if(msg.content.toLowerCase() === "beast"){
     msg.reply('SASHIKANT is Co founder of Arktic Clan');
 }
-else if(msg.content.toLowerCase().includes('chutiya'))
+else if(msg.content.toLowerCase() === "!monster"){
+    msg.reply('\nDownload Monster Hack\nhttp://gestyy.com/w3388u');
+}
+else if(msg.content.toLowerCase() === "!creation"){
+    msg.reply('\nDownload Creation Hack\nhttps://cdn.discordapp.com/attachments/615456178562138153/615679362058027024/imgui_8-26-2019.exe');
+}
+else if(msg.content.toLowerCase() === "!degogh"){
+    msg.reply('\nDownload Degogh from\nhttps://linktr.ee/degogh');
+}
+else if(msg.content.toLowerCase() === "!fendi"){
+    msg.reply('\nDownload Fendi from \nhttp://gestyy.com/w33rN2');
+}
+else if(msg.content.toLowerCase() === "!kamikaze"){
+    msg.reply('\nDownload Kamikaze from \nhttp://corneey.com/w38HwL');
+}
+else if(msg.content.toLowerCase() === "!lamborghini"){
+    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!lb"){
+    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!lifevip"){
+    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
+}
+else if(msg.content.toLowerCase() === "!mh"){
+    msg.reply('\ndownload memory hackers from \nhttps://mega.nz/#!wVMQHAzJ!jYfqHDKVpm5CCQUjN6dnaAzIJ8AmLQjueRT2BTu9ouk');
+}
+else if(msg.content.toLowerCase() === "!sinki"){
+    msg.reply('\nDownload Sinki from \nhttps://www.sinkimobile.com/download');
+}
+else if(msg.content.toLowerCase() === "!vnhax"){
+    msg.reply('\nDownload vn hax from \nhttps://cdn.discordapp.com/attachments/592650786782314501/611159206824771586/VnHaxLoader-Win64-Shipping_4.exe');
+}
+else if(msg.content.toLowerCase() === "!youtube"){
+    msg.reply('HEY THIS IS MY YOUTUBE CHANNEL\nSUBSCRIBE AND GET DAILY HACK GAMEPLAY VIDEO\nhttps://www.youtube.com/channel/UC083eydMbfgPuHMja6rte9Q');
+}
+else if(msg.content.toLowerCase() === "!venom"){
+    msg.reply('\nDownload Venom hack from\nhttps://mshare.io/file/Ycsmzp');
+}
+else if(msg.content.toLowerCase() === "!life"){
+    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
+}
+else if(msg.content.toLowerCase() === "!lif"){
+    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
+}
+else if(msg.content.toLowerCase() === "!errorx"){
+    msg.reply('\nDownload Errorx from \nhttps://cdn.discordapp.com/attachments/609707272527937537/617370377114353694/errorXvip.vmp.safe.exe');
+}
+{
+if (msg.channel.id === '546001221442600972' || msg.channel.id === '598531387963736082' || msg.channel.id === '610513586560696330' || msg.channel.id === '545986441306570791' || msg.channel.id === '609788620236390441' || msg.channel.id === '597772282441629709' || msg.channel.id === '597737539004268544' || msg.channel.id === '597737604615503873' || msg.channel.id === '597737699788455937' || msg.channel.id === '597740807507935232' || msg.channel.id === '597741089688125449' || msg.channel.id === '597741500989833226' || msg.channel.id === '597741588579614730' || msg.channel.id === '597741665796489228' || msg.channel.id === '598012142477901824' || msg.channel.id === '598416934387580939' || msg.channel.id === '598436459644583936' || msg.channel.id === '597666558667653136' || msg.channel.id === '597667306838949918' || msg.channel.id === '536885367270408204' || msg.channel.id === '538775389334536204' || msg.channel.id === '538775433601220612' || msg.channel.id === '587499819694555156' || msg.channel.id === '590456120490065930' || msg.channel.id === '590826417458249728' || msg.channel.id === '617770964259831847' || msg.channel.id === '613368968190754819' || msg.channel.id === '616199386707525662' || msg.channel.id === '613024175698870279' || msg.channel.id === '616276019212714005' || msg.channel.id === '613024057436143628' || msg.channel.id === '616928899007512576' || msg.channel.id === '615557684124450817' || msg.channel.id === '492377472671940625' || msg.channel.id === '617118406243778605'){
+if(msg.content.toLowerCase().includes('chutiya'))
     {
         msg.reply('BBhen ke lawde gaand na fulao maa chood denge tumhari');
     }
@@ -172,51 +222,6 @@ else if(msg.content.toLowerCase().includes('chutia'))
     {
         msg.reply('Bhen ke lawde gaand na fulao maa chood denge tumhari');
     }
-else if(msg.content.toLowerCase() === "!monster"){
-    msg.reply('\nDownload Monster Hack\nhttp://gestyy.com/w3388u');
-}
-else if(msg.content.toLowerCase() === "!creation"){
-    msg.reply('\nDownload Creation Hack\nhttps://cdn.discordapp.com/attachments/615456178562138153/615679362058027024/imgui_8-26-2019.exe');
-}
-else if(msg.content.toLowerCase() === "!degogh"){
-    msg.reply('\nDownload Degogh from\nhttps://linktr.ee/degogh');
-}
-else if(msg.content.toLowerCase() === "!fendi"){
-    msg.reply('\nDownload Fendi from \nhttp://gestyy.com/w33rN2');
-}
-else if(msg.content.toLowerCase() === "!kamikaze"){
-    msg.reply('\nDownload Kamikaze from \nhttp://corneey.com/w38HwL');
-}
-else if(msg.content.toLowerCase() === "!lamborghini"){
-    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
-}
-else if(msg.content.toLowerCase() === "!lb"){
-    msg.reply('\nDownload Lamborghini hack from \nhttps://www.sinkimobile.com/download');
-}
-else if(msg.content.toLowerCase() === "!lifevip"){
-    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
-}
-else if(msg.content.toLowerCase() === "!mh"){
-    msg.reply('\ndownload memory hackers from \nhttps://mega.nz/#!wVMQHAzJ!jYfqHDKVpm5CCQUjN6dnaAzIJ8AmLQjueRT2BTu9ouk');
-}
-else if(msg.content.toLowerCase() === "!sinki"){
-    msg.reply('\nDownload Sinki from \nhttps://www.sinkimobile.com/download');
-}
-else if(msg.content.toLowerCase() === "!vnhax"){
-    msg.reply('\nDownload vn hax from \nhttps://cdn.discordapp.com/attachments/592650786782314501/611159206824771586/VnHaxLoader-Win64-Shipping_4.exe');
-}
-else if(msg.content.toLowerCase() === "!youtube"){
-    msg.reply('HEY THIS IS MY YOUTUBE CHANNEL\nSUBSCRIBE AND GET DAILY HACK GAMEPLAY VIDEO\nhttps://www.youtube.com/channel/UC083eydMbfgPuHMja6rte9Q');
-}
-else if(msg.content.toLowerCase() === "!venom"){
-    msg.reply('\nDownload Venom hack from\nhttps://mshare.io/file/Ycsmzp');
-}
-else if(msg.content.toLowerCase() === "!life"){
-    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
-}
-else if(msg.content.toLowerCase() === "!lif"){
-    msg.reply('\nDownload Lifevip hack from \nhttp://gestyy.com/w33yf3');
-}
 else if(msg.content.toLowerCase().includes('loda'))
     {
         msg.reply('Bhaag na motherchod idhar maa chudane kyo aaya');
@@ -233,9 +238,6 @@ else if(msg.content.toLowerCase().includes('loode'))
     {
         msg.reply('Bhaag na motherchod idhar maa chudane kyo aaya');
     }
-else if(msg.content.toLowerCase() === "!errorx"){
-    msg.reply('\nDownload Errorx from \nhttps://cdn.discordapp.com/attachments/609707272527937537/617370377114353694/errorXvip.vmp.safe.exe');
-}
 else if(msg.content.toLowerCase().includes('fuck'))
     {
         msg.reply('Get Lost and Fuck yourself Bitch');
@@ -476,6 +478,7 @@ else if(msg.content.toLowerCase().includes('lavdee'))
     {
         msg.reply('Bhaag na motherchod idhar maa chudane kyo aaya');
     }
+    }}
 }
 })
 
